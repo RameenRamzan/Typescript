a915ef32ac7db90e3597cfc4bd5b0889fb74927c
RameenRamzan <161635962+RameenRamzan@users.noreply.github.com>
2024-03-11 00:39:29 +0500

    Added personal_Message

---
+++ personal_Message/app.ts
@@ -0,0 +1,5 @@
+// Project 02
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let personName :string = "Eric";
+console.log("Hello! "+personName+",How do you feel being a part of Governor IT Initiative?");
+

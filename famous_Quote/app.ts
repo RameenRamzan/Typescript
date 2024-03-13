f1616eed03aa60574ed09e5c47d5bed42cf1693f
RameenRamzan <161635962+RameenRamzan@users.noreply.github.com>
2024-03-13 22:28:09 +0500

    ADDED famous_Quote

---
+++ famous_Quote/app.ts
@@ -0,0 +1,5 @@
+// PROJECT 04:
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let personName : string = "Albert Einstein";
+console.log(personName +' once said'+','+'"The measure of intelligence is the ability to change."' );

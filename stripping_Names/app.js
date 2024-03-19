d7f5711023edd5be8817f180730ea132f34dfbb7
RameenRamzan <161635962+RameenRamzan@users.noreply.github.com>
2024-03-20 01:43:06 +0500

    ADDED stripping_Names

---
+++ stripping_Names/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Project 07
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// White Space:
+let personName = "\n\t Rameen Ramzan \t\n";
+console.log(personName);
